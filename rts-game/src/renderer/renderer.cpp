87da52bf502cd8222188f6232d6c02815703c9f8
Vizaxo <26389755+Vizaxo@users.noreply.github.com>
2025-05-12 20:25:44 +0100

    Only get backbuffer rtv once

--- rts-game/src/renderer/renderer.cpp
+++ rts-game/src/renderer/renderer.cpp
@@ -10,9 +10,27 @@
 ID3D11Device* device;
 ID3D11DeviceContext* context;
 IDXGISwapChain* swapchain;
+ID3D11RenderTargetView* backbuffer_rtv;
 Terrain terrain({100, 100});
 
 Microsoft::WRL::Wrappers::RoInitializeWrapper* initialize;
+
+ID3D11RenderTargetView* get_backbuffer_rtv() {
+	DXGI_SWAP_CHAIN_DESC sc_desc;
+	swapchain->GetDesc(&sc_desc);
+	ID3D11Texture2D* backbuffer;
+	HRASSERT(swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&backbuffer));
+
+	D3D11_RENDER_TARGET_VIEW_DESC rtv_desc {};
+	rtv_desc.Format = sc_desc.BufferDesc.Format;
+	rtv_desc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
+	rtv_desc.Texture2D.MipSlice = 0;
+
+	ID3D11RenderTargetView* rtv;
+	HRASSERT(device->CreateRenderTargetView(backbuffer, &rtv_desc, &rtv));
+	assert(rtv);
+	return rtv;
+}
 
 void renderer_init(App &app) {
 	device = app.device;
@@ -34,23 +52,8 @@ void renderer_init(App &app) {
     if (FAILED(hr))
 		debug_print(LogLevel::FATAL, "Could not initialise WIC");
 #endif
-}
 
-ID3D11RenderTargetView* get_backbuffer_rtv() {
-	DXGI_SWAP_CHAIN_DESC sc_desc;
-	swapchain->GetDesc(&sc_desc);
-	ID3D11Texture2D* backbuffer;
-	HRASSERT(swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&backbuffer));
-
-	D3D11_RENDER_TARGET_VIEW_DESC rtv_desc {};
-	rtv_desc.Format = sc_desc.BufferDesc.Format;
-	rtv_desc.ViewDimension = D3D11_RTV_DIMENSION_TEXTURE2D;
-	rtv_desc.Texture2D.MipSlice = 0;
-
-	ID3D11RenderTargetView* backbuffer_rtv;
-	HRASSERT(device->CreateRenderTargetView(backbuffer, &rtv_desc, &backbuffer_rtv));
-	assert(backbuffer_rtv);
-	return backbuffer_rtv;
+	backbuffer_rtv = get_backbuffer_rtv();
 }
 
 void set_viewport() {
